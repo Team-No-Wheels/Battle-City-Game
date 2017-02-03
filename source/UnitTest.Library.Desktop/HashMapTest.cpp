16d6546712a69f35490a9d429e8e15dbb54fc22b
Sanjay Nambiar <sanjay.mnambiar@gmail.com>
2017-02-03 10:49:07 -0500

    Renamed classes and added test classes

---
+++ source/UnitTest.Library.Desktop/HashMapTest.cpp
@@ -0,0 +1,41 @@
+#include "Pch.h"
+#include "HashMapTestTemplate.h"
+#include "TestClassHelper.h"
+#include "ToStringTemplates.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLibraryDesktop
+{
+	TEST_CLASS(HashMapTest)
+	{
+	public:
+		TEST_METHOD(TestDefaultConstructor)
+		{
+		}
+
+		TEST_CLASS_INITIALIZE(InitializeClass)
+		{
+			mHelper.BeginClass();
+		}
+
+		TEST_METHOD_INITIALIZE(Setup)
+		{
+			mHelper.Setup();
+		}
+
+		TEST_METHOD_CLEANUP(Teardown)
+		{
+			mHelper.Teardown();
+		}
+
+		TEST_CLASS_CLEANUP(CleanupClass)
+		{
+			mHelper.EndClass();
+		}
+	private:
+		static TestClassHelper mHelper;
+	};
+
+	TestClassHelper HashMapTest::mHelper;
+}
---
+++ source/UnitTest.Library.Desktop/HashMapTestTemplate.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CppUnitTestAssert.h"
+#include "HashMap.h"
+
+namespace UnitTestLibraryDesktop
+{
+	using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+	template <typename T>
+	class HashMapTestTemplate
+	{
+	public:
+		static void TestDefaultConstructor()
+		{
+		}
+	};
+}
