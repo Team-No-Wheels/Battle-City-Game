f14fd6cc149233f41e18fc4a95f9c442101eb8d9
Sanjay Nambiar <sanjay.mnambiar@gmail.com>
2017-03-25 15:53:58 -0400

    Added doxygen comments

--- source/Library.Shared/Factory.h
+++ source/Library.Shared/Factory.h
@@ -6,6 +6,9 @@
 
 namespace AnonymousEngine
 {
+	/** This class is a templated abstract factory class.
+	 *  There are macros defined which lets the user create custom factories
+	 */
 	template <typename AbstractProductT>
 	class Factory : public RTTI
 	{
@@ -13,16 +16,43 @@ namespace AnonymousEngine
 	public:
 		typedef typename FactoryMap::Iterator FactoryIterator;
 
+		/** Create an instance of the concrete product instance which this factory supports
+		 *  @return An instance of the concrete product
+		 */
 		virtual AbstractProductT* Create() const = 0;
+		/** Create an abstract product which this factory hierrarchy supports
+		 *  @return The name of the concrete product class which this factory creates
+		 */
 		virtual std::string ClassName() const = 0;
 
+		/** Finds the factory registered for a given name
+		 *  @param name The name against which the registered factory is to be searched
+		 *  @return If found return the address of the factory instance else return null
+		 */
 		static Factory<AbstractProductT>* Find(const std::string& name);
+		/** Creates an instance of a concrete product based on the given product name
+		 *  @param name The name of the concrete product type
+		 *  @return An instance of the concerete product specified by name
+		 */
 		static AbstractProductT* Create(const std::string& name);
 		
+		/** Gets the begining of the factories list
+		 *  @return The begin iterator of the factories list
+		 */
 		static FactoryIterator begin();
+		/** Gets the end of the factories list
+		 *  @return The end iterator of the factories list
+		 */
 		static FactoryIterator end();
 		
+		/** Register a factory against a product name
+		 *  @param factory The factory which is to be registered
+		 */
 		static void Add(Factory<AbstractProductT>& factory);
+		/** Remove a factory against a product name
+		 *  @param factory The factory which is to be removed
+		 *  @return True if factory was removed, false otherwise
+		 */
 		static bool Remove(Factory<AbstractProductT>& factory);
 	private:
 		static FactoryMap Factories;
