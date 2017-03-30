64004d9f5a31f4c965a77ae288cedfd20ab07b66
Sanjay Nambiar <sanjay.mnambiar@gmail.com>
2017-03-30 18:55:38 -0400

    Integrated chrono based game clock and game time

---
+++ source/Library.Shared/GameTime.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <chrono>
+
+namespace AnonymousEngine
+{
+	class GameTime final
+	{
+	public:
+		GameTime();
+
+		const std::chrono::high_resolution_clock::time_point& CurrentTime() const;
+		void SetCurrentTime(const std::chrono::high_resolution_clock::time_point& currentTime);
+		
+		const std::chrono::milliseconds& TotalGameTime() const;
+		void SetTotalGameTime(const std::chrono::milliseconds& totalGameTime);
+
+		const std::chrono::milliseconds& ElapsedGameTime() const;		
+		void SetElapsedGameTime(const std::chrono::milliseconds& elapsedGameTime);
+
+	private:
+		std::chrono::high_resolution_clock::time_point mCurrentTime;
+		std::chrono::milliseconds mTotalGameTime;
+		std::chrono::milliseconds mElapsedGameTime;
+	};
+}
