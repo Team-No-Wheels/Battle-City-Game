92d1bc545c6a5a17ec3089646a78c33866390a3f
Sanjay Nambiar <sanjay.mnambiar@gmail.com>
2017-03-30 21:19:18 -0400

    Added empty test classes for world, sector, entity and action

---
+++ source/UnitTest.Library.Desktop/EntityTest.cpp
@@ -0,0 +1,42 @@
+#include "Pch.h"
+#include "TestClassHelper.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+namespace UnitTestLibraryDesktop
+{
+	using namespace AnonymousEngine;
+
+	TEST_CLASS(EntityTest)
+	{
+	public:
+		TEST_METHOD(TestInitialize)
+		{
+
+		}
+		TEST_CLASS_INITIALIZE(InitializeClass)
+		{
+			mHelper.BeginClass();
+		}
+
+		TEST_METHOD_INITIALIZE(Setup)
+		{
+			mHelper.Setup();
+		}
+
+		TEST_METHOD_CLEANUP(Teardown)
+		{
+			mHelper.Teardown();
+		}
+
+		TEST_CLASS_CLEANUP(CleanupClass)
+		{
+			mHelper.EndClass();
+		}
+
+		static TestClassHelper mHelper;
+	};
+
+	TestClassHelper EntityTest::mHelper;
+}
