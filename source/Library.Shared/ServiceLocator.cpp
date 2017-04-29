bc11ee5154d9af36426cd1622c2cb7987d2b464d
Plato Manchi <plato.manchi@gmail.com>
2017-04-29 19:03:19 -0400

    Created ServiceLocator.

---
+++ source/Library.Shared/ServiceLocator.cpp
@@ -0,0 +1,36 @@
+#include "Pch.h"
+#include "ServiceLocator.h"
+
+namespace AnonymousEngine
+{
+	namespace Core
+	{
+		HashMap<ServiceLocator::ServiceType, Service*> ServiceLocator::sServicesCache;
+
+		Service* ServiceLocator::GetService(ServiceType pServiceType)
+		{
+			HashMap<ServiceType, Service*>::Iterator itr = sServicesCache.Find(pServiceType);
+			if (itr != sServicesCache.end())
+			{
+				return itr->second;
+			}
+			// if service is not found
+			return nullptr;
+		}
+
+		Graphics::TextureLoaderService* ServiceLocator::GetTextureLoader()
+		{
+			return static_cast<Graphics::TextureLoaderService*>(GetService(ServiceType::TextureLoader));
+		}
+
+		Graphics::RendererService* ServiceLocator::GetRenderer()
+		{
+			return static_cast<Graphics::RendererService*>(GetService(ServiceType::Renderer));
+		}
+
+		void ServiceLocator::AddService(ServiceType pServiceType, Service& pService)
+		{
+			sServicesCache[pServiceType] = &pService;
+		}
+	}
+}
