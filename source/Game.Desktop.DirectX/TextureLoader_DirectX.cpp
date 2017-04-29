9dba543de2c6f34d1dbf1937d7a335df922cb3d2
Plato Manchi <plato.manchi@gmail.com>
2017-04-29 19:12:03 -0400

    Updating the vcxproj and filter file for TextureLoader_Direct files

--- source/Game.Desktop.DirectX/TextureLoader_DirectX.cpp
+++ source/Game.Desktop.DirectX/TextureLoader_DirectX.cpp
@@ -5,6 +5,26 @@ namespace AnonymousEngine
 {
 	namespace Graphics
 	{
+		TextureLoader_DirectX::TextureLoader_DirectX()
+		{
 
+		}
+
+		TextureLoader_DirectX::~TextureLoader_DirectX()
+		{
+			Purge();
+		}
+
+		std::uint32_t TextureLoader_DirectX::GetTexture(const std::string& pTextureFilePath, bool pForceLoad)
+		{
+			// TODO : Load texture using DirectX
+			return 0U;
+		}
+
+		void TextureLoader_DirectX::Purge()
+		{
+			// TODO : delete texture form GPU
+			mTextureBank.Clear();
+		}
 	}
 }
