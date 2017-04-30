552179b4e9f62e8f85099c11f6fd972662b9f5a1
Siddhant Grover <siddhantgrover94@hotmail.com>
2017-04-30 01:29:30 -0400

    - Added Collider class (Incomplete).

---
+++ source/Library.Shared/Collider.cpp
@@ -0,0 +1,34 @@
+#include "Collider.h"
+
+using namespace AnonymousEngine::Containers;
+
+namespace AnonymousEngine
+{
+	Collider::Collider(GameObject& gameObject) : mGameObject(gameObject), mColliderTag(ColliderTag::Invalid), mHeight(0), mLength(0)
+	{
+
+	}
+
+	void Collider::SetTag(const Collider::ColliderTag& tag)
+	{
+		mColliderTag = tag;
+	}
+
+	const Collider::ColliderTag& Collider::GetTag() const
+	{
+		return mColliderTag;
+	}
+	
+	void Collider::SetDimensions(const std::int32_t& length, const std::int32_t& height)
+	{
+		mLength = length;
+		mHeight = height;
+	}
+	
+	bool Collider::InCollision(const Collider& otherCollider)
+	{
+		// TODO Implement
+		otherCollider;
+		return false;
+	}
+}
