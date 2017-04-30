7281565c24077d7b38c9c4d7dbab16a57188bcfc
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-04-30 00:51:30 -0400

    Initial commit of Water.h, Water.cpp, LevelManager.h and LevelManager.cpp

---
+++ source/BattleCity/Water.cpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "Pch.h"
+#include "Water.h"
+
+namespace BattleCity
+{
+	namespace MapEntities
+	{
+		ATTRIBUTED_DEFINITIONS(Water)
+		ENTITY_FACTORY_DEFINITIONS(Water)
+	}
+}
