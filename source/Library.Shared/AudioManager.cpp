de77657db5945da2f0f36598fb1ec532d30b4a54
Gaurav Nanda <gauravnba@gmail.com>
2017-04-30 13:47:31 -0500

    Build working with AudioManager. Note that AudioManager is a dummy class at the moment

--- source/Library.Shared/AudioManager.cpp
+++ source/Library.Shared/AudioManager.cpp
@@ -1,9 +1,17 @@
+#include "Pch.h"
 #include "AudioManager.h"
 
 using namespace AnonymousEngine::Audio;
 
 AnonymousEngine::HashMap<std::string, std::string> mSoundMap = { {"Collisions", ""} };
 
-void AudioManager::Update(const Containers::EventMessageAttributed& message)
+namespace AnonymousEngine
 {
+	namespace Audio
+	{
+		void AudioManager::Update(const Containers::EventMessageAttributed& message)
+		{
+			UNREFERENCED_PARAMETER(message);
+		}
+	}
 }
