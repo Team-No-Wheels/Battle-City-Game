2d26dea0866832ed4d882a31c31f2db7c30fffa3
Plato Manchi <plato.manchi@gmail.com>
2017-04-30 22:03:09 -0400

    Renderable is no longer Entity Added Frame class which is of type Entity

---
+++ source/Library.Shared/SpriteFrame.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace AnonymousEngine
+{
+	namespace Graphics
+	{
+		class SpriteFrame
+		{
+		public:
+			std::string mKey;
+			std::uint32_t x, y, width, height;
+		};
+	}
+}
---
+++ source/Library.Shared/SpriteSheet.cpp
@@ -0,0 +1,22 @@
+#include "Pch.h"
+#include "SpriteSheet.h"
+
+
+namespace AnonymousEngine
+{
+	namespace Graphics
+	{
+		SpriteSheet::SpriteSheet(Core::GameObject& pGameObject) :
+			 Sprite(pGameObject)
+		{
+
+		}
+
+		SpriteSheet::~SpriteSheet()
+		{
+
+		}
+
+
+	}
+}
